7bed632461660ee577e4bd4ec6c5e2d9aaa164b5
tomasmcm <tomasmcm@whitesmith.co>
2022-08-16 21:38:17 +0100

    add linkwallets test

--- tests/MittenLink_test.sol
+++ tests/MittenLink_test.sol
@@ -50,4 +50,27 @@ contract MittenLinkTest {
             "0xFFfF... should have value match max transfer"
         );
     }
+
+    function linkWallets () public {
+        try mittenLinkToTest.linkWallets(
+            0x890EaCEB4eE2e893f9155Ddf45887885Ba7963f5,
+            0x2E5deB91b444EfbeA95E34BFb9aA043A5F99f567,
+            hex"02ed0403848da68320848da6833a825208942e5deb91b444efbea95e34bfb9aa043a5f99f5678656156ba0c40080c0",
+            27,
+            0x45fef98df3f09150c788ad8ac6cf04ade04c6666d45fc77617d9cf68e5808f33,
+            0x0e75977861e736be01d0af5a7c8d518250bfbed7176c6a7848b6435b7f4902ad
+        ) {
+            console.log("linkWallets ok");
+        } catch Error(string memory) {
+            Assert.ok(false, "failed with reason");
+        } catch (bytes memory) {
+            Assert.ok(false, "failed unexpected");
+        }
+
+        address[] memory walletLinks = mittenLinkToTest.getWalletLinks(0x2E5deB91b444EfbeA95E34BFb9aA043A5F99f567);
+
+        Assert.equal(walletLinks.length, 1, "Wallet links should have 1 address exactly");
+
+        Assert.equal(walletLinks[0], 0x890EaCEB4eE2e893f9155Ddf45887885Ba7963f5, "Wallet link does not match expected address");
+    }
 }
